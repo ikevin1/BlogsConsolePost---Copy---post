10559adf9b75358d3d5ff0abab6d52bec51eb28c
ikevin <ikevin@my.wctc.edu>
2019-03-28 20:11:23 -0700

    Added Post

---
+++ BlogsConsole/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BlogsConsole.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+
+        //public static implicit operator Post(Post v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
